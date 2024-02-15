de162c45318389e44f70474f8a4dfd27cb7fd4c6
Justin Simon <justinsimon@Justins-MBP.lan>
2024-02-15 11:31:29 -0800

    Making new project

---
+++ vidly_app2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace vidly_app2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
